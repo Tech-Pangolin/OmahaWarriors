538c5699ce3fdd7b4d953eb20358c88986551939
Terra Taylor <terra.taylor@gmail.com>
2023-08-25 13:25:49 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,93 @@
 import logo from './logo.svg';
 import './App.css';
-import Todo from './Todo/Todo';
+import {db} from '../firebase';
+import React, { useState, useEffect } from 'react';
+import { collection, addDoc, getDocs } from "firebase/firestore";
 
 function App() {
+  const addTodo = async (e) => {
+    e.preventDefault();  
+   
+    try {
+        const docRef = await addDoc(collection(db, "todos"), {
+          todo: todo,    
+        });
+        console.log("Document written with ID: ", docRef.id);
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+}
+
+const fetchPost = async () => {
+   
+    await getDocs(collection(db, "todos"))
+        .then((querySnapshot)=>{              
+            const newData = querySnapshot.docs
+                .map((doc) => ({...doc.data(), id:doc.id }));
+                console.log(newData)
+            setTodos(newData);                
+            console.log(todos, newData);
+        })
+   
+}
+
+useEffect(()=>{
+    fetchPost();
+}, [fetchPost])
+
+    const team = [
+      {
+      name: 'Bobby Johnson',
+      number: 2
+    },
+      {
+      name: 'Carter Anderson',
+      number: 8
+    },
+      {
+      name: 'Easton Post',
+      number: 10
+    },
+      {
+      name: 'Emerson Nuñez',
+      number: 13
+    },
+      {
+      name: 'Jack Peteler',
+      number: 3
+    },
+      {
+      name: 'Jackson Chidester',
+      number: 23
+    },
+      {
+      name: 'Michael Wittry',
+      number: 12
+    },
+      {
+      name: 'Parker Allen',
+      number: 7
+    },
+      {
+      name: 'Rex Rosseter',
+      number: 9
+    },
+      {
+      name: 'Vincent Cummings III',
+      number: 5
+    },
+  
+  ]
+
+
+
   return (
     <div>
     <header id="header" className="fixed-top">
     <div className="container d-flex align-items-center">
 
       {/* <h1 className="logo me-auto"><a href="index.html">Mentor</a></h1> */}
-     <a href="index.html" className="logo me-auto"><img src="assets/img/baseball/logo.jpg" alt="" className="img-fluid" /></a>
+     <a href="index.html" className="logo me-auto"><img src="./img/baseball/logo.jpg" alt="" className="img-fluid" /></a>
 
       <nav id="navbar" className="navbar order-first order-lg-0">
         <ul>
@@ -47,6 +125,7 @@ function App() {
     </div>
   </header>
   <section id="hero" className="d-flex justify-content-center align-items-center">
+  
     <div className="container position-relative" data-aos="zoom-in" data-aos-delay="100">
       <h1>Omaha Warriors<br/>Baseball</h1>
       {/* <h2>We are team of talented designers making websites with Bootstrap</h2> */}
@@ -61,18 +140,18 @@ function App() {
           <p>Support our Team</p>
         </div>
 
-        <div className="row" data-aos="zoom-in" data-aos-delay="100">
-
-          <div className="col-lg-4 col-md-6 d-flex align-items-stretch">
+        <div className="row d-flex justify-content-center" data-aos="zoom-in" data-aos-delay="100">
+          {team.map((player, i) =>
+          <div className="col-lg-3 col-md-3 d-flex align-items-stretch justify-content-center mb-5">
             <div className="course-item">
-              <img src="assets/img/course-1.jpg" className="img-fluid" alt="..."/>
+              <img src="./img/75378.jpeg" className="img-fluid" alt="..."/>
               <div className="course-content">
                 <div className="d-flex justify-content-between align-items-center mb-3">
-                  <h4>#2</h4>
+                  <h4>#{player.number}</h4>
                   <p className="price">$169</p>
                 </div>
 
-                <h3><a href="course-details.html">Bobby Johnson</a></h3>
+                <h3><a href="course-details.html">{player.name}</a></h3>
                 <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
                 <div className="trainer d-flex justify-content-between align-items-center">
                   <div className="trainer-profile d-flex align-items-center">
@@ -88,65 +167,15 @@ function App() {
               </div>
             </div>
           </div>
-
-          <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
-            <div className="course-item">
-              <img src="assets/img/course-2.jpg" className="img-fluid" alt="..."/>
-              <div className="course-content">
-                <div className="d-flex justify-content-between align-items-center mb-3">
-                  <h4>#8</h4>
-                  <p className="price">$250</p>
-                </div>
-
-                <h3><a href="course-details.html">Carter Anderson</a></h3>
-                <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
-                <div className="trainer d-flex justify-content-between align-items-center">
-                  <div className="trainer-profile d-flex align-items-center">
-                    <img src="assets/img/trainers/trainer-2.jpg" className="img-fluid" alt=""/>
-                    <span>Sample</span>
-                  </div>
-                  <div className="trainer-rank d-flex align-items-center">
-                    <i className="bx bx-user"></i>&nbsp;35
-                    &nbsp;&nbsp;
-                    <i className="bx bx-heart"></i>&nbsp;42
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> 
-
-          <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
-            <div className="course-item">
-              <img src="assets/img/course-3.jpg" className="img-fluid" alt="..." />
-              <div className="course-content">
-                <div className="d-flex justify-content-between align-items-center mb-3">
-                  <h4>#10</h4>
-                  <p className="price">$180</p>
-                </div>
-
-                <h3><a href="course-details.html">Easton Poast</a></h3>
-                <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
-                <div className="trainer d-flex justify-content-between align-items-center">
-                  <div className="trainer-profile d-flex align-items-center">
-                    <img src="assets/img/trainers/trainer-3.jpg" className="img-fluid" alt=""/>
-                    <span>Brandon</span>
-                  </div>
-                  <div className="trainer-rank d-flex align-items-center">
-                    <i className="bx bx-user"></i>&nbsp;20
-                    &nbsp;&nbsp;
-                    <i className="bx bx-heart"></i>&nbsp;85
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          
+          
+          )}
+          
 
         </div>
 
       </div>
     </section>
-
-        <Todo />
         
     </div>
   );
